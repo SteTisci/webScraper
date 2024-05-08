e3cb803c9a0fb01b6d3090f5c0c682b0162b7164
SteTisci <ste.tisci@gmail.com>
2024-05-08 17:25:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+const url = 'https://www.ebay.com/';
+const valRicerca = 'nvidia rtx 3080ti';
+
+const scraper = async (url, valRicerca, elementi, nome, prezzo) => {
+    const browser = await puppeteer.launch();
+    const pagina = await browser.newPage();
+
+    await pagina.goto(url);
+    await pagina.locator('input[type="text"]').fill(valRicerca);
+    await pagina.locator('input[type="submit"]').click();
+
+    await pagina.waitForNavigation();
+
+    const risultati = await pagina.evaluate((selettoreElementi, selettoreNome, selettorePrezzo) => {
+        const articoli = document.querySelectorAll(selettoreElementi);
+
+        return Array.from(articoli).map((articolo) => {
+            const nome = articolo.querySelector(selettoreNome).innerText.replace('NEW LISTING','').trim();
+            const prezzo = articolo.querySelector(selettorePrezzo).innerText.replace('EUR', '€');
+
+            return { nome, prezzo };
+        });
+    }, elementi, nome, prezzo);
+
+    console.log(risultati);
+
+    await browser.close();
+};
+
+scraper(url, valRicerca, '.s-item__info', '.s-item__title', '.s-item__price');
