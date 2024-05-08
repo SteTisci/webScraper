7373dff50b879830b3fe8ee25dd7cfc0df04dca9
SteTisci <ste.tisci@gmail.com>
2024-05-09 01:44:30 +0200

    Aggiunto stato e link prodotti

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const url = 'https://www.ebay.com/';
 const valRicerca = 'nvidia rtx 3080ti';
 
-const scraper = async (url, valRicerca, elementi, nome, prezzo) => {
+const scraper = async (url, valRicerca, elementi, nome, prezzo, stato, link) => {
     const browser = await puppeteer.launch();
     const pagina = await browser.newPage();
 
@@ -13,16 +13,18 @@ const scraper = async (url, valRicerca, elementi, nome, prezzo) => {
 
     await pagina.waitForNavigation();
 
-    const risultati = await pagina.evaluate((selettoreElementi, selettoreNome, selettorePrezzo) => {
+    const risultati = await pagina.evaluate((selettoreElementi, selettoreNome, selettorePrezzo, selettoreStato, selettoreUrl) => {
         const articoli = document.querySelectorAll(selettoreElementi);
 
         return Array.from(articoli).map((articolo) => {
             const nome = articolo.querySelector(selettoreNome).innerText.replace('NEW LISTING','').trim();
             const prezzo = articolo.querySelector(selettorePrezzo).innerText.replace('$', '');
+            const stato = articolo.querySelector(selettoreStato).innerText;
+            const link = articolo.querySelector(selettoreUrl).href;
 
-            return { nome, prezzo };
+            return { nome, prezzo, stato, link };
         });
-    }, elementi, nome, prezzo);
+    }, elementi, nome, prezzo, stato, link);
 
     risultati.sort((a, b) => a.prezzo - b.prezzo);
     console.log(risultati);
@@ -31,4 +33,4 @@ const scraper = async (url, valRicerca, elementi, nome, prezzo) => {
 
 };
 
-scraper(url, valRicerca, '.s-item__info', '.s-item__title', '.s-item__price');
+scraper(url, valRicerca, '.s-item__info', '.s-item__title', '.s-item__price', '.SECONDARY_INFO', 'a[href]');
